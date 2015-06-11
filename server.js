b1ae1b9699f511adc93e19a9a084fdd03ba33127
Justin Peterson <Sleepy@Justins-MacBook-Pro.local>
2015-06-10 23:24:52 -0700

    redid node server

--- server.js
+++ server.js
@@ -1,66 +1,21 @@
-var connect = require('connect')
-  , express = require('express')
-  , http = require('http')
-  , request = require('request')
-  , serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
-
-
-
+var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+//var multer = require('multer'); 
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-
-    // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//app.use(multer()); // for parsing multipart/form-data
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.post('/getcoords', function (req, res) {
+  console.log(req.body);
+  res.json(req.body);
+})
 
 
-
-//response.send(request);
-	//build querystring
-
-	console.log(request.body);
-	//console.log(request.params.city);
-	//request.params.street
-    //request.params.City
-    //request.params.State
-
-
-//	request('https://maps.googleapis.com/maps/api/geocode/xml?address=' + querystring + '=AIzaSyDm4xGlr4ptZ-7Cx8OGqfDQy-6DBBjrEbQ', function (error, response, body) {
- //	 if (!error && response.statusCode == 200) {
-	//	    console.log(body) // Show the HTML for the Google homepage.
-  //}
-  
-});
-
-app.get('/derp', function (req, res) {
-  res.send(rq);
-});
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.json(req.body);
+})
